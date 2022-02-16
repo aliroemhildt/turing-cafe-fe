b7addd46d00fae522005da5c85ae7c7adc2e95f2
Ali Roemhildt <aliroemhildt@gmail.com>
2022-02-16 09:41:38 -0700

    Create card component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,1 +1,19 @@
+import React from 'react';
 
+const Card = ({ id, name, date, time, number }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>Number of guests: {number}</p>
+      <button
+        id={id}
+      >
+        Cancel
+      </button>
+    </div>
+  )
+}
+
+export default Card;
