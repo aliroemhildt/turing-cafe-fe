5351d42bc9bb3898b4173179f7a1284d55fa0cb1
Ali Roemhildt <aliroemhildt@gmail.com>
2022-02-16 09:28:09 -0700

    Start building out Reservations component

---
+++ src/Components/Reservations/Reservations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Reservations = () => {
+  const allReservations =
+  // need to fetch reservations in app and pass them down
+
+  return (
+    <section>
+      { allReservations }
+    </section>
+  );
+}
